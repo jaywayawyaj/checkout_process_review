322a0696d3b824596d2e225d7ec9a274057f3de0
Jethro Whaley <jethro.whaley@gmail.com>
2019-03-16 10:39:29 +0000

    refactor: checkout method only 4 lines long, extracted everything I can

--- lib/shop.rb
+++ lib/shop.rb
@@ -7,36 +7,37 @@ class Shop
       'A' => 0,
       'B' => 0
     }
-
   end
 
   SKUS = {
-    'A' => {:amount => 50, deal_amount: 3, discount: 20},
-    'B' => {:amount => 30, deal_amount: 2, discount: 15},
-    'C' => {:amount => 20},
-    'D' => {:amount => 15}
+    'A' => {amount: 50, deal_amount: 3, discount: 20},
+    'B' => {amount: 30, deal_amount: 2, discount: 15},
+    'C' => {amount: 20},
+    'D' => {amount: 15}
   }
 
   def checkout(basket)
     total_reset
-
     return -1 unless basket.is_a? String
-    basket.split('').each do |product|
-      return -1 unless SKUS.include?(product)
-      calculate_price(product)
-      product_counter(product)
-    end
-    calculate_discount
-    counter_reset
+    sort_basket(basket)
     @total
   end
 
   private
 
+  def sort_basket(basket)
+    basket.split('').each do |product|
+      return @total = -1 unless SKUS.include?(product)
+      calculate_price(product)
+    end
+    calculate_discount
+  end
+
   def calculate_price(product)
     SKUS.each do |sku, value|
       @total += value[:amount] if product == sku
     end
+    product_counter(product)
   end
 
   def counter_reset
@@ -46,13 +47,16 @@ class Shop
   end
 
   def calculate_discount
-    @total -= (@counters['A']/SKUS['A'][:deal_amount] * SKUS['A'][:discount])
-    @total -= (@counters['B']/SKUS['B'][:deal_amount] * SKUS['B'][:discount])
+    @counters.each do |sku|
+      @total -=
+        @counters[sku[0]]/SKUS[sku[0]][:deal_amount] * SKUS[sku[0]][:discount]
+    end
+    counter_reset
   end
 
   def product_counter(product)
-    @counters.each do |p, c|
-      if p == product
+    @counters.each do |sku|
+      if sku[0] == product
         @counters[product] += 1
       end
     end
